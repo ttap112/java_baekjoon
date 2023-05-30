bcd5cf97f57548992b658620403e62954b349394
Mirage <86884419+ttap112@users.noreply.github.com>
2023-05-30 16:23:05 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 18052 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner Scan = new Scanner(System.in);
+		
+		double a = Scan.nextDouble();
+		double b = Scan.nextDouble();
+				
+		
+		System.out.println(a/b);
+	}
+
+}
