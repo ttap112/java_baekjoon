fee2399b33c1846067466c4db396a7c79d412dec
Mirage <86884419+ttap112@users.noreply.github.com>
2023-05-30 16:26:48 +0900

    [Bronze V] Title: 사칙연산, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner Scan = new Scanner(System.in);
+		
+		int a = Scan.nextInt();
+		int b = Scan.nextInt();
+				
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
